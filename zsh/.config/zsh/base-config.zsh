e306f5c05fe6da1bd30aeaeffd7a60d9683f1471
Michael Barlow <michael@ramyun.co.uk>
2025-09-24 14:07:59 +0200

    enable menu selection in zsh config

--- zsh/.config/zsh/base-config.zsh
+++ zsh/.config/zsh/base-config.zsh
@@ -10,3 +10,7 @@ autoload -Uz compinit && compinit
 # Use vim keybindings
 bindkey -v
 
+# Enable menu selection (plus shift-tab to go backwards)
+zstyle ':completion:*' menu select
+bindkey "$terminfo[kcbt]" reverse-menu-complete
+setopt menucomplete
