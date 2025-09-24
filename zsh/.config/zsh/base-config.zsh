1c2aec37b02c774d5cff8dbea924cab98a5b0af5
Michael Barlow <michael@ramyun.co.uk>
2025-09-24 13:17:22 +0200

    add zsh aliases and git aliases

--- zsh/.config/zsh/base-config.zsh
+++ zsh/.config/zsh/base-config.zsh
@@ -1,6 +1,9 @@
 # Configure command line prompt
 source $HOME/.config/zsh/prompt.zsh
 
+# Load custom aliases
+source $HOME/.config/zsh/aliases.zsh
+
 # Enable command auto-completion
 autoload -Uz compinit && compinit
 
